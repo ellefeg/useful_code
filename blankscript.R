cb8305ee6a3afafa518f697b1c57f1a01dd844eb
Laura Grice <ellefeg@users.noreply.github.com>
2020-03-25 10:56:24 +1000

    Update blankscript.R

--- blankscript.R
+++ blankscript.R
@@ -26,20 +26,28 @@ myInterestGenes <- args[2]
 myBackgroundGenes <- args[3]
 myAnnotations <- args[4]
 myDescriptions <- args[5]
-
-bioc_packages <- c("multtest")
+# load packages
+bioc_packages <- c("DESeq2", "pheatmap", "apeglm", "genefilter")
+r_packages <- c("RColorBrewer", "ggplot2", "pheatmap")
+## function to load R packages
+baseRpkgTest <- function(x) {
+  if (!suppressMessages(require(x,character.only = TRUE, quietly = T))) {
+    install.packages(x,dep=TRUE, repos = "https://pbil.univ-lyon1.fr/CRAN/")
+    if(!require(x,character.only = TRUE, quietly = T)) stop (paste0(x, "package not found"))
+  }
+}
 ## function to load bioconductor packages
 biocondpkgTest <- function(x) {
-  if (!suppressMessages(require(x,character.only = TRUE, quietly = T))) {
+  if (!require(x,character.only = TRUE)) {
     source("http://www.bioconductor.org/biocLite.R")
     biocLite(x)
-    if(!require(x,character.only = TRUE, quietly = T)) stop (paste0(x, "bioconductor package not found"))
+    if(!require(x,character.only = TRUE)) stop (paste0(x, "bioconductor package not found"))
   }
 }
 ## load packages
 for (b_pkg in bioc_packages) {
   biocondpkgTest(b_pkg)
-  sink("citations.txt", append=TRUE)
-  print(citation(b_pkg))
-  sink()
+}
+for (r_pkg in r_packages) {
+  baseRpkgTest(r_pkg)
 }
